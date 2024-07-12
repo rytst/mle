0588ba724027672aeac8d58e7a45b8fda0999808
rytst <r.saito0111@gmail.com>
2024-07-12 15:22:22 +0900

    first commit

---
+++ hist/hist.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+path = os.path.join(os.path.dirname(__file__), '..', 'height.txt')
+
+# load text data
+xs = np.loadtxt(path)
+
+mu = np.mean(xs)
+sigma = np.std(xs)
+
+
+# normal distribution
+def normal(x, mu=0, sigma=1):
+    y = 1 / (np.sqrt(2 * np.pi) * sigma) * np.exp(- (x - mu)**2 / (2 * sigma**2))
+    return y
+
+
+x = np.linspace(150, 190, 1000)
+y = normal(x, mu, sigma)
+
+
+
+plt.hist(xs, bins='auto', density=True)
+plt.plot(x, y)
+plt.xlabel('Height(cm')
+plt.ylabel('Probabilty Density')
+plt.savefig('fit.png')
+
+
